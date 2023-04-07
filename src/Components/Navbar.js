c5bd44811c20d7ee075de8533c5281b62888de65
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-07 20:50:40 +0100

    nav section added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag } from "react-icons/ai";
 import { BsFillCartFill, BsFillSaveFill } from "react-icons/bs";
 import { TbTruckDelivery } from "react-icons/tb";
@@ -6,10 +6,12 @@ import { FaUserFriends, FaWallet } from "react-icons/fa";
 import { MdFavorite, MdHelp } from "react-icons/md";
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState(false)
     return (
         <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
             <div className="flex items-center">
-                <div className="cursor-pointer">
+                <div onClick={()=>setNav(!nav)} className="cursor-pointer">
                     <AiOutlineMenu size={30} />
                 </div>
                 <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
@@ -27,9 +29,9 @@ const Navbar = () => {
             <button className="bg-black text-white hidden md:flex items-center py-2 rounded-full">
                 <BsFillCartFill size={20} className="mr-2" /> Cart
             </button>
-            <div className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div>
-            <div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300">
-               <AiOutlineClose size={30} className="absolute right-4 top-4 cursor-pointer" />
+            {nav ? <div onClick={()=>setNav(!nav)} className="bg-black/80 fixed w-full h-screen z-10 top-0 left-0"></div> : ''}
+            {nav ? <div className="fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300">
+               <AiOutlineClose onClick={()=>setNav(!nav)} size={30} className="absolute right-4 top-4 cursor-pointer" />
                <h2 className="text-2xl p-4">
                    Best <span className="font-bold">Eats</span>
                </h2>
@@ -44,7 +46,7 @@ const Navbar = () => {
                        <li className="text-xl py-4 flex"><FaUserFriends size={25} className="mr-4" /> Invite Friends</li>
                    </ul>
                </nav>
-            </div>
+            </div> : ""}
         </div>
     );
 };
