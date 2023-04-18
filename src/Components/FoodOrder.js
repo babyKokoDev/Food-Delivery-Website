c13936035248e160401bc746c0c4994fcc09bcf2
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-18 13:04:35 +0100

    order now

--- src/Components/FoodOrder.js
+++ src/Components/FoodOrder.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { data } from '../data/data'
 import { useParams } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import {AiOutlinePlus, AiOutlineMinus} from 'react-icons/ai'
 
 const FoodOrder = () => {
+    const [num, setNum] = useState(1)
+    const increase = () => {
+        setNum(num + 1)
+    }
+    const decrease = () => {
+       num > 1 ? setNum(num - 1) : setNum(1)   
+    }
     const params = useParams()
     const userIndex =  `${params.foodId}`
     const userData = data[userIndex-1]
@@ -20,8 +28,15 @@ const FoodOrder = () => {
                   <div>
                     <img src={userData.image} className='w-full h-[400px] object-cover' alt="" />
                   </div>
-                  <div className='text-center'>
-                    <p className='text-slate-700 text-3xl'>{userData.name}</p>
+                  <div className='text-center px-2'>
+                    <p className='text-slate-700 text-3xl mb-3'>{userData.name}</p>
+                    <div className='flex justify-center items-center'>
+                         <span onClick={decrease} className='cursor-pointer hover:bg-slate-500 p-2'><AiOutlineMinus  size={30} /></span>
+                         <span className='mx-4 border-slate-700 border px-5 text-2xl'>{num}</span>
+                         <span onClick={increase} className='cursor-pointer hover:bg-slate-500 p-2'><AiOutlinePlus  size={30}/></span>
+                    </div>
+                    <div className='mt-4 w-[50%] mx-auto bg-slate-400 shadow-lg py-2 text-white text-2xl'>${userData.price*num}</div>
+                    <button className='mt-4 w-[50%] mx-auto bg-[#D75127] shadow-lg py-2 text-white border-none rounded-none hover:scale-105 text-2xl'>Buy it now</button>
                   </div>
               </div>
           </div>
--- src/data/data.js
+++ src/data/data.js
@@ -5,70 +5,80 @@ export const data = [
         name : 'Amala',
         category : 'Swallow',
         image : 'https://media.istockphoto.com/id/1398349488/photo/amala-and-ewedu.jpg?s=612x612&w=0&k=20&c=-DTj5xJ2tCCTiLj4iTXi8n1iTa23yjvQchmPxt09V4c=',
-        price : 12
+        price : 12,
+        description : 'Àmàlà is a Nigerian food made out of yamflour. Yams are peeled, sliced, cleaned, dried and then blended into a flour, also called elubo. Yams are white in colour but turn brown when dried; this gives àmàlà its colour. Àmàlà is from Western Africa and eaten mostly by the Yoruba people in Nigeria. This food can be consumed with ewedu (jute) and stew (made from pepper, tomato, palm oil, water). The full health benefits of this kind of meal is good for the wellbeing of humans.rich in a lot of health benefits including aiding weight loss and lowering cancer risks'
     },
     {
         id : 2,
         name : 'Salad',
         category : 'Light Food',
         image : 'https://images.pexels.com/photos/764925/pexels-photo-764925.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        price : 5
+        price : 5,
+        description : 'A salad is a dish consisting of mixed, mostly natural ingredients. They are typically served at room temperature or chilled, though some can be served warm. Condiments and salad dressings, which exist in a variety of flavors, are often used to enhance a salad. Garden salads use a base of leafy greens such as lettuce, arugula/rocket, kale or spinach; they are common enough that the word salad alone often refers specifically to garden salads. Other types include bean salad, tuna salad, bread salad (e.g. fattoush, panzanella), vegetable salads without leafy greens (e.g. Greek salad, potato salad, coleslaw), sōmen salad (a noodle-based salad), fruit salad, and desserts like jello salad.'
     },
     {
         id : 3,
         name : 'Pepper soup',
         category : 'Light Food',
         image : 'https://lagosbukagrillandbar.com/wp-content/uploads/2021/07/pepper-soup-goat-meat.jpeg',
-        price : 35
+        price : 35,
+        description : 'African pepper soup is a hearty African soup made with Goat meat cutlets or chunks, pepper soup spice, and other beautiful condiments.  If you are looking for an authentic Goat meat pepper soup, this soup certainly delivers the big, bold African flavors you will love. The African Pepper soup is primarily liquid, usually served hot and this is made by boiling Meat or Fish of choice in order to extract flavor from them. The broth from this Meat is then further seasoned with authentic African spices. It can be served with boiled Potatoes, Rice, Bread or just eat ”as is”.'
     },
     {
         id : 4,
         name : 'Jollof Rice',
         category : 'Heavy Food',
         image : 'https://flawlessfood.co.uk/wp-content/uploads/2023/01/Jollof-Rice-04.jpg',
-        price : 15
+        price : 15,
+        description : 'Jollof rice is a staple in West African cuisine. It’s made from rice, tomatoes, onions, peppers, and other seasonings. The dish is cooked in one pot. It’s simple and easy to make at home—and the end result is absolutely delicious!, it is a rich and incredibly rich, aromatic, tasty West African one-pot Meal. It’s similar to Jambalaya but with distinctive African spices. It consists mainly of cooked rice and tomato stew flavored with spices such as thyme, scotch bonnet pepper, onions, and garlic. The dish is a staple of West African cuisine, particularly that of Ghana, Nigeria, Senegal, and Gambia often eaten and enjoyed during holidays, weddings, birthdays, and other special events.'
     },
     {
         id : 5,
         name : 'Fried Rice',
         category : 'Heavy Food',
         image : 'https://allnigerianfoods.com/wp-content/uploads/fried_rice_recipe.jpg',
-        price : 25
+        price : 25,
+        description : 'Fried rice is a dish of cooked rice that has been stir-fried in a wok or a frying pan and is usually mixed with other ingredients such as eggs, vegetables, seafood, or meat. It is often eaten by itself or as an accompaniment to another dish. Fried rice is a popular component of East Asian, Southeast Asian and certain South Asian cuisines, as well as a staple national dish of Indonesia. As a homemade dish, fried rice is typically made with ingredients left over from other dishes, leading to countless variations. Fried rice first developed during the Sui Dynasty in China and as such all fried rice dishes can trace their origins to Chinese fried rice'
     },
     {
         id : 6,
         name : 'Chicken',
         category : 'Light Food',
         image : 'https://images.pexels.com/photos/616354/pexels-photo-616354.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        price : 17
+        price : 17,
+        description : 'Fried chicken, also known as Southern fried chicken, is a dish consisting of chicken pieces that have been coated with seasoned flour or batter and pan-fried, deep fried, pressure fried, or air fried. The breading adds a crisp coating or crust to the exterior of the chicken while retaining juices in the meat. Broiler chickens are most commonly used. The first dish known to have been deep fried was fritters, which were popular in the European Middle Ages. However, the Scottish were the first Europeans to deep fry their chicken in fat. Meanwhile, many West African peoples had traditions of seasoned fried chicken (though battering and cooking the chicken in palm oil). Scottish frying techniques and West African seasoning techniques were combined by enslaved Africans and African Americans in the American South.'
     },
     {
         id : 7,
         name : 'Apple',
         category : 'Fruits',
         image : 'https://images.pexels.com/photos/102104/pexels-photo-102104.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-        price : 9
+        price : 9,
+        description : 'Apples are rich in fiber, vitamins, and minerals, all of which benefit human health. They also provideTrusted Source an array of antioxidants. These substances helpTrusted Source neutralize free radicals. Free radicals are reactive molecules that can build up as a result of natural processes and environmental pressures. If too many free radicals accumulate in the body, they can cause oxidative stress. This can lead to cell damage. Cell damage can contribute to a range of conditions, including cancer and diabetes.Apples are an important sourceTrusted Source of antioxidants considering their widespread consumption, particularly in Northern Europe and the United States.'
     },
     {
         id : 8,
         name : 'Eba',
         category : 'Swallow',
         image : 'https://cdn.thenet.ng/wp-content/uploads/2016/09/eba-400x391.jpg',
-        price : 31
+        price : 31,
+        description : 'Ẹ̀bà also known as Utara (in Nigeria) or Pinon (in Togo, Benin, and southern Ghana) is a staple swallow from Nigeria, also eaten in the West African sub-region and other African countries. The term èbà originates from the Yoruba people of southwest Nigeria. It is also called Utara by the Igbo People of Southeast Nigeria. It is a cooked starchy vegetable food made from dried grated cassava (manioc) flour commonly known as garri. It is often eaten with rich soups and stews, with beef, stockfish or mutton. The dish is often described as having a slightly sour, sharp taste. Èbà/ Utara is eaten with the fingers, rolled into a small ball, and dipped into thick soups such as okra soup, bitter leaf soup or with either okro, ọgbọnọ (Igbo)/ apọn (Yorùbá), or ewédú, meat or fish, stewed vegetables or other sauces such as gbẹ̀gìrì, Amiedi (banga soup) or egusi soup. In West Africa, there two types of garri, the white and yellow; the yellow garri is prepared by frying with the addition of palm oil to give it a yellow colour, white garri is fried without palm oil.[7][8]'
     },
     {
         id : 9,
         name : 'Bread & Egg',
         category : 'Light Food',
         image : 'https://www.foodnetwork.com/content/dam/images/food/fullset/2020/02/10/0/FNK_One-Pan-Egg-Cheese-Omelet-Sandwich_H1_s4x3.jpg',
-        price : 7
+        price : 7,
+        description : 'Bread is a staple food prepared from a dough of flour (usually wheat) and water, usually by baking. Throughout recorded history and around the world, it has been an important part of many cultures diet. It is one of the oldest human-made foods, having been of significance since the dawn of agriculture, and plays an essential role in both religious rituals and secular culture. Bread may be leavened by naturally occurring microbes (e.g. sourdough), chemicals (e.g. baking soda), industrially produced yeast, or high-pressure aeration, which creates the gas bubbles that fluff up bread. In many countries, commercial bread often contains additives to improve flavor, texture, color, shelf life, nutrition, and ease of production.'
     },
     {
         id : 10,
         name : 'Pounded Yam',
         category : 'Swallow',
         image : 'https://dnbstories.com/wp-content/uploads/2021/05/pounded-yam-and-soup-1024x691.jpg',
-        price : 32
+        price : 32,
+        description : 'Pounded Yam is a staple in many West African homes, but it is not usually eaten by itself, it is often paired with many delicious African soups, and stews like Egusi, okra soup, Jute leaves soup (Ewedu), or Stewed Spinach (Efo Riro). If you are new to this food, I’ll explain. Pounded is quite similar to mashed potatoes but it has a more doughy consistency. It belongs to a group of food we refer to as swallow because of the way we eat it. We cut out a morsel from the meal,  then we form an indentation on it with the thumb. Then, we scoop some stew or soup over it and we eat it.'
     },
     {
         id : 11,
